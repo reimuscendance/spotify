7ef5ee0a7a6da863a2c84f4cd07f0bec476f612c
reimuscendance <117093020+reimuscendance@users.noreply.github.com>
2023-06-06 15:52:01 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pprint
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy.util as util
 import pandas as pd
+import json
+import matplotlib.pyplot as plt
 
 # export SPOTIPY_CLIENT_SECRET="4b2a58733e6a40c4b221d44d83f9d81a"
 #export SPOTIPY_CLIENT_ID="ed6fe433e91b43a79490bea46ef85b00"
@@ -20,9 +22,18 @@ token = util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, re
 sp = spotipy.Spotify(auth=token)    
 access_token = token
 
+def request(endpoint: str) -> dict:
+    resp = requests.get(
+        "https://api.spotify.com/v1/" + endpoint,
+        headers={'Authorization': 'Bearer ' + token}
+    )
+
+    resp.raise_for_status()
+    return resp.json()
 
 def artist_search():
-    artist_name = str(input("Please Enter the artists name you want to see"))
+    artist_name = input("Please Enter the artists name you want to see")
+
 
     results = sp.search(q=artist_name, type='artist')
     items = results['artists']['items']
@@ -48,11 +59,8 @@ def artist_search():
 
 def song_search():
     # Search for the artist
-    search_query = str(input("Please Enter the song you want to see: "))
-    search_type = 'track'
-    search_url = f'https://api.spotify.com/v1/search?q={search_query}&type={search_type}'
-    response = requests.get(search_url, headers={'Authorization': 'Bearer ' + token})
-    response_json = response.json()
+    search_query = input("Please Enter the song you want to see: ")
+    response_json = request(f'search?q={search_query}&type=track')
 
     # Get the URI
     track_uri = response_json['tracks']['items'][0]['uri']
@@ -64,23 +72,16 @@ def song_search():
     sp.start_playback()
     
 def data_panda():
-    headers={'Authorization': 'Bearer ' + token}
-    search_query = str(input("Please Enter the song you want to see: "))
-    search_type = 'track'
-    search_url = f'https://api.spotify.com/v1/search?q={search_query}&type={search_type}'
-    response = requests.get(search_url, headers={'Authorization': 'Bearer ' + token})
-    response_json = response.json()
+    search_query = input("Please Enter the song you want to see: ")
+    response_json = request(f'search?q={search_query}&type=track')
 
     # Get the URI
-    track_uri = response_json['tracks']['items'][0]['uri']
-    print(track_uri)
-    base_url = "https://api.spotify.com/v1/"
-    r = requests.get(base_url + 'audio-features/' + track_uri, headers=headers)
-    r = r.json()
-    r
+    track_id = response_json['tracks']['items'][0]['id']
+    resp_json = request('audio-features/' + track_id)
+    print(pd.json_normalize(resp_json))
 
 
-menu = input("Please select an option:\n1. Search for an artist\n2. Search for a song\n3.")
+menu = input("Please select an option:\n1. Search for an artist\n2. Search for a song\n3.Graph for song\n")
 if menu == "1":
     artist_search()
 if menu == "2":
